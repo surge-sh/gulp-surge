fcc32465777ab466cb65090ce8303070e75ca40d
Brent Vatne <brentvatne@gmail.com>
2015-12-26 11:52:29 -0800

    Remove commented requires

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict'
 
-// var gutil = require('gulp-util')
 var spawn = require('child_process').spawn
 var path = require('path')
-// var surge = require('surge')
 var surge = path.resolve(path.dirname(require.resolve('surge')), '../../.bin/surge' + (process.platform === 'win32' ? '.cmd' : ''))
 
 module.exports = function (options) {
